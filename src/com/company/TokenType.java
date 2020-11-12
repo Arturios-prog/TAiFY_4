9cdb018d12a9f4e43c1cf5d5480318dd0bff8aae
Artem <fedodeev1artem@gmail.com>
2020-11-12 23:39:56 +0300

    commit 1

---
+++ src/com/company/TokenType.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public enum TokenType {
+    SPACES,         // пробел
+    ADD,            // символ "+"
+    SUB,            // символ "-"
+    MUL,            // символ "*"
+    DIV,            // символ "/"
+    EXCLAM,         // символ "!"
+    SEMICOLON,      // символ ";"
+    WHILE,
+    SIXTEEN,        // шестандцатиричное число
+    DO,
+    INCR,           // инкремент
+    DECR,           // декремент
+    LPAR,           // символ "("
+    RPAR,           // символ ")"
+    LFIGBR,         // символ "{"
+    RFIGBR,         // символ "}"
+    VAR,            // переменная
+    EQUATE,         // приравнивание
+    ASSIGN,         // присваивание
+    MORE,           // больше
+    LESS,           // меньше
+    EQUAL,          // равно
+    MORE_OR_EQUAL,  // больше или равно
+    LESS_OR_EQUAL,  // меньше или равно
+    NOT_EQUAl,       // не равно
+
+}
+
